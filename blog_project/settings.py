1bb8d4c426cf8004eda2f99d4759f9808ed3006a
ellinashurovsk <lina.shurovskaya@yandex.ru>
2022-06-30 11:47:09 +0200

    Heroku adjustments

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-django_heroku.settings(locals)
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
